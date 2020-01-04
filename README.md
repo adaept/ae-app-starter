efface42330738db6d53aea8ee65026e38f82d2e
peterennis <peterennis@yahoo.com>
2020-01-04 12:45:33 -0800

    initial commit v0.0.1 Stencil 1.8.3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ae-app-starter
+
+Built with Stencil 1.8.3
---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,25 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: true
+})
+export class AppHome {
+
+  render() {
+    return (
+      <div class='app-home'>
+        <p>
+          adaept app starter with <a href='https://stenciljs.com'>Stencil</a>
+        </p>
+
+        <stencil-route-link url='/profile/stencil'>
+          <button>
+            Profile page
+          </button>
+        </stencil-route-link>
+      </div>
+    );
+  }
+}
